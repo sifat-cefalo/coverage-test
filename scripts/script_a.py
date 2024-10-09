d5a434ffb1f137efb65e159b35e2ad4311587385
sifat-cefalo <sifat.hassan@cefalo.com>
2024-10-09 15:45:17 +0600

    Initial branch

---
+++ scripts/script_a.py
@@ -0,0 +1,8 @@
+
+
+def sum(a: int, b: int) -> int:
+    return a + b
+
+
+def subtract(a: int, b: int) -> int:
+    return a - b
---
+++ tests/test_script_a.py
@@ -0,0 +1,5 @@
+from scripts.script_a import sum
+
+
+def test_sum():
+    assert sum(2, 3) == 5
