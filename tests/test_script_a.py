41b4a38350645df4f9cb1cbb1a5e77b7d0132006
sifat-cefalo <sifat.hassan@cefalo.com>
2024-10-10 10:55:21 +0600

    feat: upgrade sum function

--- tests/test_script_a.py
+++ tests/test_script_a.py
@@ -1,5 +1,9 @@
-from scripts.script_a import custom_sum
+from scripts.script_a import custom_sum, subtract
 
 
 def test_sum():
     assert custom_sum(2, 3) == 5
+
+
+def test_subtract():
+    assert custom_sum(5, 2) == 3
